368f962b4bc600d8874eba03a86f1b5fce3b9447
Rehy <129746514+Rpyvo@users.noreply.github.com>
2024-03-23 14:20:48 +0400

    Update portmap.py

--- portmap.py
+++ portmap.py
@@ -1,6 +1,7 @@
 import threading
 import socket
 import time
+
 print("""
   ____                   _                               
  |  _ \    ___    _ __  | |_   _ __ ___     __ _   _ __  
@@ -10,38 +11,25 @@ print("""
                                                   |_|
                                                   by Rehman. Open source project
 """)
-# Hizmetleri tanımla
-SERVICES = {
-    80: "HTTP",
-    22: "SSH",
-    530: "RPC",
-    21: "FTP",
-    23: "Telenet",
-    53: "DNS",
-    25: "SMTP",
-    194: "IRC",
-    443: "HTTPS",
-    3389: "WSH"
-}
+
+def port_servisini_bul(port_numarasi):
+    try:
+        servis_adi = socket.getservbyport(port_numarasi)
+        return servis_adi
+    except OSError:
+        return "Not result"
 
 def port_scan(target_ip, port):
-    """
-    Belirtilen IP adresindeki belirli bir portu tarar ve durumu ve hizmet tipini yazdırır.
-
-    Args:
-        target_ip (str): Hedef IP adresi.
-        port (int): Tarama yapılacak port numarası.
-    """
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((target_ip, port))
-            sock.send(b'\x19')  # SYN bayrağı ayarlanmış paket gönderilir.
+            sock.send(b'\x19') 
             response = sock.recv(1024)
 
-            if b'RST' in response:  # RST yanıtı, portun kapalı olduğunu gösterir
+            if b'RST' in response: 
                 pass
             else:
-                service = SERVICES.get(port)  # Hizmeti belirle, bilinmeyen ise "Unknown" olarak ayarla
+                service = port_servisini_bul(port)
                 print(f"Port {port}: Açık - {service}")
 
     except Exception as e:
@@ -63,23 +51,19 @@ if __name__ == "__main__":
     time.sleep(2)
     print("[$] ----------------------")
 
-    # İş parçacığı sayısını belirleyin (isteğe göre ayarlayın)
     num_threads = thread
 
-    start_time = time.time()  # Başlama zamanını kaydedin
+    start_time = time.time() 
 
     threads = []
     for port in range(port_start, port_end + 1):
-        # Yeni bir iş parçacığı oluşturun ve başlatın
         thread = threading.Thread(target=port_scan, args=(target_ip, port))
         threads.append(thread)
         thread.start()
-
-    # Tüm iş parçacıklarının bitmesini bekleyin
+      
     for thread in threads:
-        thread.join()
-
-    # Tarama süresini hesaplayın ve yazdırın
+        thread.join() 
+      
     end_time = time.time()
     total_time = end_time - start_time
     print("[$] ----------------------")
